afb3e698497c6bb517ae43921aa3a5f675675e93
silvermistshadow <bmead72@gmail.com>
2019-10-23 13:47:24 -0700

    update letterMatch a bit

--- src/Hangman.js
+++ src/Hangman.js
@@ -6,11 +6,17 @@ export class Hangman {
   letterMatch(letter) {
     let splitWord = this.word.split('');
     letter = letter.toLowerCase();
+    let indexes = [], i;
     for (let x = 0; x<splitWord.length; x++) {
-      if (splitWord.includes(letter)) {
-        return true;
+      if (splitWord[x] === letter) {
+        indexes.push([x]);
+        console.log(indexes);
+        i = true;
+      }
+      else {
+        i = false;
       }
     }
+    return i;
   }
-  
 }
