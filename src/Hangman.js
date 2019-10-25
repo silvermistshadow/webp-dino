b5e732b5b350f3f21991b29223398b75505fd03e
silvermistshadow <bmead72@gmail.com>
2019-10-25 10:41:28 -0700

    continue working on letterCorrect method

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,15 +1,15 @@
-import { indexes } from './gvars';
+import { indexes, letterArray } from './gvars';
 export class Hangman {
   constructor(word) {
     this.word = word;
-
+    this.letterArray = new Array(this.word.length).fill("");
   }
 
   letterMatch(letter) {
     let splitWord = this.word.split('');
     letter = letter.toLowerCase();
     let i;
-    indexes = [];
+    indexes.length = 0;
     for (let x = 0; x<splitWord.length; x++) {
       if (splitWord[x] === letter) {
         indexes.push(x);
@@ -24,7 +24,10 @@ export class Hangman {
 
   letterCorrect() {
     if (indexes && indexes.length) {
-      
+      let splitWord = this.word.split('');
+      for(let x=0; x<indexes.length; x++) {
+        this.letterArray.push(splitWord[indexes[x]]);
+      }
     }
   }
 
