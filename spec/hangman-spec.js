157e20c763f7744ab616ced3566ade24be917458
silvermistshadow <bmead72@gmail.com>
2019-10-30 13:41:55 -0700

    start completely refactoring code

--- spec/hangman-spec.js
+++ spec/hangman-spec.js
@@ -7,23 +7,4 @@ describe('Hangman', function() {
   });
 
 
-  it('should use letterMatch to match a single letter in a word', function() {
-    let testLetterMatch = testWord.letterMatch("o");
-    expect(testLetterMatch).toBe(true);
-  });
-
-  it('should use letterCorrect to retrieve exactly which letters are correct and their positions', function() {
-    let testLetterMatch = testWord.letterMatch("o"); //must be done in order to use the next method
-    let testLetterCorrect = testWord.letterCorrect();
-    expect(testLetterCorrect).toContain("o");
-  }); 
-
-  it('should use a pair of integers to keep track of both correct letters so far and guesses left', function() {
-    testWord.hangJudge("o");
-    testWord.hangJudge("p");
-    expect(testWord.correctLetters).toEqual(1);
-    expect(testWord.gameCounter).toEqual(5);
-  });
-
-
 })
--- src/Hangman.js
+++ src/Hangman.js
@@ -1,60 +1,17 @@
 import { indexes } from './gvars';
 export class Hangman {
   constructor(word) {
-    this.word = word;
-    this.letterArray = new Array(this.word.length).fill("");
-    this.lettersGuessed = [];
-    this.correctLetters = 0;
-    this.gameCounter = 6;
-    this.letterAlreadyGuessed = false;
+    this.word = this.getWord();
+    this.guesses = 0;
+    this.blankArray = this.getBlank();
   }
 
-  isAlreadyGuessed(letter) {
-    this.letterAlreadyGuessed = this.lettersGuessed.includes(letter);
-
+  getBlank() {
+    return new Array(this.word.length).fill("")
   }
-
-  letterMatch(letter) {
-    let splitWord = this.word.split('');
-    letter = letter.toLowerCase();
-    let i;
-    indexes.length = 0;
-    for (let x = 0; x<splitWord.length; x++) {
-      if (splitWord[x] === letter) {
-        indexes.push(x);
-        i = true;
-      }
-      else {
-        i = false;
-      }
-    }
-    return i;
-  }
-
-  letterCorrect() {
-    if (indexes && indexes.length) {
-      let splitWord = this.word.split('');
-      for(let x=0; x<indexes.length; x++) {
-        this.letterArray[indexes[x]] = splitWord[indexes[x]];
-      }
-      return this.letterArray;
-    }
-  }
-
-  hangJudge(letter) {
-    if (this.isAlreadyGuessed(letter) === false) {
-      this.lettersGuessed.push(letter);
-      if (this.letterMatch(letter)) {
-        this.letterCorrect();
-        this.correctLetters += 1
-        console.log(this.correctLetters);
-      }
-      else {
-        this.gameCounter -= 1
-        console.log(this.gameCounter);
-      }
-    }
-
+  getWord() {
+    //Have to rewrite the tests later
+    return "Dino";
   }
 
 }
