398e139bd1d921ee668670e19b96ebc2a1f93920
silvermistshadow <bmead72@gmail.com>
2019-11-01 10:18:49 -0700

    try to use API and asynchrony

--- spec/hangman-spec.js
+++ spec/hangman-spec.js
@@ -1,9 +1,9 @@
 import { Hangman } from '../src/Hangman';
 
 describe('Hangman', function() {
-  let testWord = new Hangman("Dino")
+  let testWord = new Hangman();
   it('should create and store a Hangman object', function() {
-    expect(testWord.word).toBe("Dino");
+    expect(testWord.word).toMatch(/\D/);
   });
 
 
--- src/Hangman.js
+++ src/Hangman.js
@@ -1,17 +1,42 @@
-import { indexes } from './gvars';
 export class Hangman {
-  constructor(word) {
+  constructor() {
     this.word = this.getWord();
     this.guesses = 0;
-    this.blankArray = this.getBlank();
+    this.blankArray;
+    this.wordInit = 0;
+  }
+  getDino() {
+    let workAround = this; //the other thing I need to return 
+    let dinoAsync = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = 'http://dinoipsum.herokuapp.com/api/?format=text&paragraphs=1&words=1';
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        }
+        else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    dinoAsync.then(function(response) {
+      let dinoWord = response;
+      workAround.wordInit = 1;
+      let blankWait = new Array(dinoWord.length).fill("");
+      workAround.blankArray = blankWait;
+      return dinoWord;
+
+    }, function(error) {
+      $('.showErrors').text(`There was an error retrieving a random dino: ${error.message}`);
+    });
   }
 
-  getBlank() {
-    return new Array(this.word.length).fill("")
-  }
   getWord() {
-    //Have to rewrite the tests later
-    return "Dino";
+    return this.getDino();
+
   }
 
 }
